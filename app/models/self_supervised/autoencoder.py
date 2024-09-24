87cd70a7cfd7081c16b23b35319287a5574416a6
Hyemee <53588762+KIMHYEMEE@users.noreply.github.com>
2024-09-24 16:52:49 +0900

    ✅ cnn autoencoder

--- app/models/self_supervised/autoencoder.py
+++ app/models/self_supervised/autoencoder.py
@@ -1,4 +1,4 @@
-from keras.layers import Input, Dense
+from keras.layers import Input, Dense, Conv2D, MaxPooling2D, Conv2DTranspose
 from keras.models import Model
 
 # simple autoencoder
@@ -40,5 +40,62 @@ class simple_autoencoder:
         h = self.enc_h(inputs)
         outputs = self.enc_output(h)
         model = Model(inputs,outputs)
+
+        return model
+    
+
+class cnn_autoencoder:
+    def __init__(self,input_shape):
+        self.input_shape = input_shape
+        self.loss = 'binary_crossentropy'
+        self.layers()
+        self.model = self.modeling()
+    
+    def get_info(self):
+        model_info = {'input_shape':self.input_shape,
+                      'output_shape':self.input_shape,
+                      'loss':self.loss}
+        return model_info
+    
+    def layers(self):
+        self.enc_layers = [Conv2D(32, (3,3), activation='relu',padding='same'),
+                           MaxPooling2D((2,2),padding='same'),
+                           Conv2D(32, (3,3), activation='relu',padding='same'),
+                           MaxPooling2D((2,2),padding='same')
+                           ]
+        
+        self.dec_layers = [Conv2DTranspose(32, (3,3), strides=2, activation='relu', padding='same'),
+                           Conv2DTranspose(32, (3,3), strides=2, activation='relu', padding='same'),
+                           Conv2D(1, (3,3), activation='sigmoid', padding='same')
+                           ]
+
+    def modeling(self):
+        inputs = Input(self.input_shape)
+
+        # encoder
+        h = self.enc_layers[0](inputs)
+        for enc_layer in self.enc_layers[1:]:
+            h = enc_layer(h)
+        
+        # decoder
+        for dec_layer in self.dec_layers[:-1]:
+            h = dec_layer(h)
+
+        outputs = self.dec_layers[-1](h)
+
+        model = Model(inputs, outputs)
+        model.compile(loss=self.loss)
+
+        return model
+
+    def encoder(self):
+        inputs = Input(self.input_shape)
+
+        h = self.enc_layers[0](inputs)
+        for enc_layer in self.enc_layers[1:-1]:
+            h = enc_layer(h)
+        outputs = self.enc_layers[-1](h)
+
+        model = Model(inputs, outputs)
 
         return model
--- app/models/ssl_models.py
+++ app/models/ssl_models.py
@@ -10,6 +10,10 @@ def build_model(model_name):
     if model_name == 'simple_autoencoder':
         # ref: https://blog.keras.io/building-autoencoders-in-keras.html
         model = simple_autoencoder(128)
+    
+    elif model_name == 'cnn_autoencoder':
+        # ref: https://keras.io/examples/vision/autoencoder/
+        model = cnn_autoencoder((28,28,1))
 
     else:
         model = None
